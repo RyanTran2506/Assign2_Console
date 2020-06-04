efab19446fce13e56ef6dc5af1301e0ec7c635df
Phu Quy Tran <tranp6@student.douglascollege.ca>
2020-06-04 16:15:02 -0700

    Added member m2

--- src/Project.java
+++ src/Project.java
@@ -1,14 +1,29 @@
+
 import java.util.ArrayList;
 
 public class Project {
 
-	private ArrayList<Member> members;
+	
+	
 	public static void Main(String[] args) {
+		ArrayList<Member> groupMembers = new ArrayList<Member>();
 		
 		Member m1 = new Member();
 		m1.setStudentID("300300123");
 		m1.setName("Ryan");
 		m1.setGender("Male");
 		m1.setResponsibility("do code");
+		
+		groupMembers.add(m1);
+		
+		//test
+		
+		Member m2 = new Member();
+		m1.setStudentID("300300323");
+		m1.setName("Ros");
+		m1.setGender("Female");
+		m1.setResponsibility("do documents");
+		
+		groupMembers.add(m2);
 	}
 }
