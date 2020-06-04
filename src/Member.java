11d1d54b8f5596d9580429ffcb1b4638b1fdb76f
Phu Quy Tran <tranp6@student.douglascollege.ca>
2020-06-04 15:53:24 -0700

    first commit

---
+++ src/Member.java
@@ -0,0 +1,32 @@
+
+public class Member {
+	private String studentID;
+	private String name;
+	private String gender;
+	private String responsibility;
+	
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getResponsibility() {
+		return responsibility;
+	}
+	public void setResponsibility(String responsibility) {
+		this.responsibility = responsibility;
+	}
+}
---
+++ src/Project.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Project {
+
+	private ArrayList<Member> members;
+	public static void Main(String[] args) {
+		
+		Member m1 = new Member();
+		m1.setStudentID("300300123");
+		m1.setName("Ryan");
+		m1.setGender("Male");
+		m1.setResponsibility("do code");
+	}
+}
